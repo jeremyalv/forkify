d5b77f90e7d7f414912db4780cff640c0f6f42e0
Jeremy <jeremyalternative@gmail.com>
2020-11-04 22:06:44 +0700

    added methods for rendering errors in View

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,7 +30,7 @@ const controlRecipes = async function() {
 
        
   } catch (err) {
-    console.log(err);
+    recipeView.renderError()
   }
 };
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,6 +5,8 @@ console.log(Fraction);
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We couldn\'t find that recipe. Please try another one!';
+  #message = '';
 
   render(data) { 
     // A public method, so it could be called from the controller later
@@ -18,7 +20,7 @@ class RecipeView {
     this.#parentElement.innerHTML = '';
   }
 
-  renderSpinner = function() {
+  renderSpinner() {
     const markup = `
       <div class="spinner">
         <svg>
@@ -26,9 +28,35 @@ class RecipeView {
         </svg>
       </div>
     `
-    this.#parentElement.innerHTML = '';
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);  
   };
+
+  renderError(message=this.#errorMessage) {
+    const markup = `<div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-alert-triangle"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div> `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);  
+  }
+
+  renderMessage(message=this.#message) {
+    const markup = `<div class="message">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div> `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);  
+  }
 
   addHandlerRender(handler) {
   ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
